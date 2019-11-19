021b635d1c3b2c0af2a8309b0fdde6e7e4a5990f
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-19 05:33:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/\A [aeiou]/)
+  if word.match(/\A^[aeiou] | [AEIOU]/)
+    true 
+  else
+    false 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
